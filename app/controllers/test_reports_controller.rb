dda97cc10f0a63784c2e7d7d843cceafe0231ac9
Ruey Jye <rueyjye@lifull.com>
2020-06-15 12:12:37 +0900

    add before_action

--- app/controllers/test_reports_controller.rb
+++ app/controllers/test_reports_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TestReportsController < ApplicationController
+  before_action :detail_page_load, only: [:show, :update]
+
   def index
     @jobs = Job.join_with_suites
                 .select_group_concat_suites
@@ -11,21 +13,10 @@ class TestReportsController < ApplicationController
   end
 
   def show
-    @select_option =  {'Unchecked': '', 'OK': 1, 'Degradation': 2, 'Fix test script': 3, 'Checking': 4}
-    @latest_round = TestCaseResult.get_latest_round(params[:id])
-    # if round exists, use the round number
-    @round = if params[:round].nil?
-                @latest_round
-              else
-                raise ActiveRecord::RecordNotFound unless params[:round].match?(/\A[1-9][0-9]*\z/)
-                params[:round]
-              end
-    @job = Job.find(params[:id])
     @suite_data = Job.join_with_suites
                       .select_group_concat_suites
                       .group('jobs.id')
                       .find(params[:id])
-    @data_for_test_reports = TestCaseResult.get_data_for_test_reports_page(params[:id], @round)
 
     gon.passed_count = @data_for_test_reports[:stack_passed_counts]
     gon.failed_count = @data_for_test_reports[:failed_count]
@@ -38,20 +29,31 @@ class TestReportsController < ApplicationController
     else
       @get_update_target_result.update(check_status: params[:check_status], check_comment: params[:check_comment])
     end
-    @latest_round = TestCaseResult.get_latest_round(params[:job_id])
-    @round = if params[:round].nil?
-      @latest_round
-    else
-      raise ActiveRecord::RecordNotFound unless params[:round].match?(/\A[1-9][0-9]*\z/)
-      params[:round]
-    end
-    @job = Job.find(params[:job_id])
-    @data_for_test_reports = TestCaseResult.get_data_for_test_reports_page(params[:job_id], @round)
-    @select_option =  {'Unchecked': '', 'OK': 1, 'Degradation': 2, 'Fix test script': 3, 'Checking': 4}
 
     respond_to do |format|
       format.html
       format.js { render action: 'show.js.erb' }
     end
+  end
+
+  private
+
+  def detail_page_load
+    if params[:action] == "show"
+      id = params[:id]
+    else params[:action] == "update"
+      id = params[:job_id]
+    end
+    @select_option =  {'Unchecked': '', 'OK': 1, 'Degradation': 2, 'Fix test script': 3, 'Checking': 4}
+    @job = Job.find(id)
+    @latest_round = TestCaseResult.get_latest_round(id)
+    # if round exists, use the round number
+    @round = if params[:round].nil?
+                @latest_round
+              else
+                raise ActiveRecord::RecordNotFound unless params[:round].match?(/\A[1-9][0-9]*\z/)
+                params[:round]
+              end
+    @data_for_test_reports = TestCaseResult.get_data_for_test_reports_page(id, @round)
   end
 end
