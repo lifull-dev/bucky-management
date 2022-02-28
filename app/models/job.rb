8f4d0137fa5b875b290c45fe2583d4b3b63835af
hiranom15 <63398614+hiranom15@users.noreply.github.com>
2022-02-28 10:56:54 +0900

    Speed up the display of the top page. (#41)
    * Speed up the display of the top page.
    
    * Fix rubocop

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,8 +14,18 @@ class Job < ApplicationRecord
   has_many :test_cases, through: :test_case_results
   has_many :test_suites, through: :test_cases
 
-  # scope :join_with_suites, -> { joins(test_case_results: { test_case: :test_suite }) }
-  scope :join_with_suites, -> { left_outer_joins(test_case_results: { test_case: :test_suite }) }
-  scope :get_job, ->(test_category, device) { joins(test_case_results: { test_case: :test_suite }).select('jobs.*').where(test_suites: { test_category: test_category }).where(test_suites: { device: device }) }
+  scope :join_with_suites, -> { joins(test_case_results: { test_case: :test_suite }) }
+  scope :get_job, lambda { |test_category, device|
+    query = <<-SQL.squish
+      SELECT jobs.* FROM jobs
+      STRAIGHT_JOIN `test_case_results` ON `test_case_results`.`job_id` = `jobs`.`id`
+      INNER JOIN `test_cases` ON `test_cases`.`id` = `test_case_results`.`test_case_id`
+      INNER JOIN `test_suites` ON `test_suites`.`id` = `test_cases`.`test_suite_id`
+      WHERE `test_suites`.`test_category` = ? AND `test_suites`.`device` = ?
+      ORDER BY `jobs`.`id` DESC
+      LIMIT 1
+    SQL
+    find_by_sql([query, test_category, device])
+  }
   scope :select_group_concat_suites, -> { select("jobs.*, GROUP_CONCAT(DISTINCT test_suites.device separator '/') AS device, GROUP_CONCAT(DISTINCT test_suites.service separator '/') AS service, GROUP_CONCAT(DISTINCT test_suites.test_category separator '/') AS category, SUM(test_case_results.elapsed_time) AS total_time") }
 end
