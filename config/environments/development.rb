6a284b79c32a608674fcf19ea4c5809d9256bc76
Naoto Kishino <kishinonaoto@example.com>
2022-09-15 19:40:56 +0900

    Fix for m1 mac

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.serve_static_assets = true
   config.after_initialize do
     Bullet.enable = true
