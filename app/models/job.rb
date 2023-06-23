f48aa857079970f848b8874c49ddfd68843e1c4a
T-Y89 <0809taikiyamashita@gmail.com>
2023-06-23 18:17:22 +0900

    parent c8eac116a669ed061e91ef613e45330ed679b700 author T-Y89 <0809taikiyamashita@gmail.com> 1687511842 +0900 committer T-Y89 <0809taikiyamashita@gmail.com> 1688020303 +0900
    ransackでラベル絞り込み機能
    
    follow rails_best_practice
    
    follow_rails_best_practice
    
    command_and_optionから検索
    
    Fix link for highlight
    
    delete unused method
    
    WIPこれだとtrが40個生成される
    
    WIP
    
    大枠完成
    
    SQLの結果を変数に入れる
    
    per_pageは不変なので定数に
    
    fix
    
    間違って消したransackを復活
    
    スタイル適応
    
    rubocop
    
    rails_best_practice
    
    rubocop

--- app/models/job.rb
+++ app/models/job.rb
@@ -28,4 +28,60 @@ class Job < ApplicationRecord
     find_by_sql([query, test_category, device])
   }
   scope :select_group_concat_suites, -> { select("jobs.*, GROUP_CONCAT(DISTINCT test_suites.device separator '/') AS device, GROUP_CONCAT(DISTINCT test_suites.service separator '/') AS service, GROUP_CONCAT(DISTINCT test_suites.test_category separator '/') AS category, SUM(test_case_results.elapsed_time) AS total_time") }
+  def self.all_root_jobs
+    Job.all.where("command_and_option not like '%rerun%'").order('jobs.id DESC')
+  end
+
+  def self.searched_root_jobs(search_word)
+    all_root_jobs.where('command_and_option LIKE ?', "%#{search_word}%")
+  end
+
+  def self.get_searched_root_jobs(start_num, per_page, search_word, page)
+    [Job.join_with_suites(Job.all_root_jobs
+            .searched_root_jobs(search_word)
+            .select(&:id)[start_num...start_num + per_page]),
+     Kaminari.paginate_array(Job.all_root_jobs
+       .searched_root_jobs(search_word).to_a,
+                             total_count: Job.all_root_jobs.searched_root_jobs(search_word).length)
+             .page(page).per(per_page)]
+  end
+
+  def self.root_jobs(start_num, per_page, page)
+    [Job.join_with_suites(Job.all_root_jobs.to_a
+        .map(&:id)[start_num...start_num + per_page]),
+     Kaminari.paginate_array(Job.all_root_jobs.to_a, total_count: Job.all_root_jobs.length).page(page).per(per_page)]
+  end
+
+  def self.all_children_jobs(start, limit)
+    Job.all.where("command_and_option like '%rerun%'").where(id: start..).order('jobs.id ASC').limit(limit)
+  end
+
+  def self.children_jobs(start, limit)
+    Job.join_with_suites(
+      # `+1` is for getting id which is more than specific id
+      Job.all_children_jobs(start + 1, limit).to_a.map(&:id)
+    )
+  end
+
+  def self.create_job_tree(parent_jobs, children_jobs)
+    job_tree = {}
+    parent_jobs.each do |parent_job|
+      job_tree[parent_job.id] = {
+        id: parent_job.id,
+        job_start_time: parent_job.start_time,
+        command_and_option: parent_job.command_and_option,
+        device: parent_job.device,
+        service: parent_job.service,
+        category: parent_job.category,
+        total_time: parent_job.total_time,
+        children: []
+      }
+
+      children_jobs.each do |child_job|
+        job_tree[parent_job.id][:children] << child_job.id if parent_job.id == TestReport.get_parent(child_job.command_and_option)
+      end
+    end
+
+    job_tree
+  end
 end
