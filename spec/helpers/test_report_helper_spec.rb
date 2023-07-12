243441c8e094e5bd20751581f143323b1f61abe1
T-Y89 <0809taikiyamashita@gmail.com>
2023-07-12 09:39:03 +0900

    change method name & delete comments

--- spec/helpers/test_report_helper_spec.rb
+++ spec/helpers/test_report_helper_spec.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-# require 'app/helpers/test_report_helper.rb'
 
 RSpec.describe TestReportHelper, type: :helper do
   describe '正常系のテスト' do
     it 'ripository以降のリンクが削除されていること' do
       url = 'https://github.com/hoge/fuga/blob/master'
-      assert_equal 'https://github.com/hoge/fuga', owner_and_ripository(url)
+      assert_equal 'https://github.com/hoge/fuga', github_repo_base_url(url)
     end
   end
 end
