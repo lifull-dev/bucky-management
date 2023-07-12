49fe9a40d0c6c4e8e1516190f8ff4338bf40dcbb
T-Y89 <0809taikiyamashita@gmail.com>
2023-07-12 09:39:03 +0900

    change method name & delete comments

--- app/helpers/test_report_helper.rb
+++ app/helpers/test_report_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module TestReportHelper
-  def owner_and_ripository(full_path)
+  def github_repo_base_url(full_path)
     full_path.sub(%r{(https://github.com/[^/]+/[^/]+)/.*}, '\1')
   end
 end
