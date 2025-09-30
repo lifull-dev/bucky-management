b20d9fbaa9b9a1c76fe833d50ef922b3738078a4
abiru-shouta <abirushouta@lifull.com>
2025-09-30 18:01:10 +0900

    feat: skip機能追加、seedsやfixturesは更新なし

--- app/assets/javascripts/test_reports_show.js
+++ app/assets/javascripts/test_reports_show.js
@@ -2,19 +2,22 @@ if (gon.controller_name === 'test_reports' && gon.action_name === 'show') {
   new Chart('test-report-detail-chart', {
     type: 'pie',
     data: {
-      labels: ['Passed', 'Failed'],
+      labels: ['Passed', 'Failed','Skipped'],
       datasets: [{
         data: [
           gon.passed_count,
           gon.failed_count,
+          gon.skipped_count,
         ],
         backgroundColor: [
           'rgba(75, 192, 192, 0.5)',
           'rgba(255, 99, 132, 0.5)',
+          'rgba(255, 206, 86, 0.5)',
         ],
         borderColor: [
           'rgba(75, 192, 192, 1)',
           'rgba(255,99,132,1)',
+          'rgba(255, 206, 86, 1)',
         ],
         borderWidth: 1,
       }],
