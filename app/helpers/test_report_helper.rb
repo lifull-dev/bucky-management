0803e3c567e00fb5fad450bdb1ac203ebf9248dd
KangMinJun <kangminjun@lifull.com>
2026-07-13 16:12:42 +0900

    Revert "feature: add PR link & 環境変数"
    This reverts commit c48011ee6705b012e9c56c67e1d8039d81aedace.

--- app/helpers/test_report_helper.rb
+++ app/helpers/test_report_helper.rb
@@ -38,41 +38,4 @@ module TestReportHelper
   def device_options
     [['All Device', ''], ['SP', 'sp'], ['PC', 'pc']]
   end
-
-  # Extract PR number from base_fqdn (e.g. "https://www-test12826.develop.homes.co.jp" -> "12826")
-  def extract_pr_number(base_fqdn)
-    return nil if base_fqdn.blank?
-
-    match = base_fqdn.match(/www-test(\d+)\./)
-    match ? match[1] : nil
-  end
-
-  # Determine repository name from command_and_option
-  def detect_repo_name(command_and_option)
-    return nil if command_and_option.blank?
-
-    if command_and_option.include?('work-in-homes-pc')
-      'homes-pc'
-    elsif command_and_option.include?('work-in-homes-sp')
-      'homes-sp'
-    end
-  end
-
-  # Generate GitHub PR link URL
-  def github_pr_url(base_fqdn, command_and_option)
-    pr_number = extract_pr_number(base_fqdn)
-    repo = detect_repo_name(command_and_option)
-    org_base_url = ENV['GITHUB_ORG_BASE_URL']
-    return nil if pr_number.nil? || repo.nil? || org_base_url.blank?
-
-    "#{org_base_url}/#{repo}/pull/#{pr_number}"
-  end
-
-  # Generate issue search URL for a given case name
-  def issue_search_url(case_name)
-    base = ENV['ISSUE_SEARCH_BASE_URL']
-    return nil if base.blank? || case_name.blank?
-
-    "#{base}?q=is%3Aissue%20state%3Aopen%20%20#{ERB::Util.url_encode(case_name)}"
-  end
 end
